08915b140e02bcfdf8638a87d8699d99413b1621
dedhiaja-12 <jashdedhia@student.lccc.wy.edu>
2025-04-13 19:36:11 -0400

    Update genLogFile.py

--- genLogFile.py
+++ genLogFile.py
@@ -3,13 +3,22 @@ import random
 import datetime
 
 def generate_log_entry(ip_address, url, status_code):
-    """Generates a sample log entry."""
+    """
+    Generates a single log entry line in the following format:
+    YYYY-MM-DD HH:MM:SS - IP_ADDRESS - "GET /some/url HTTP/1.1" STATUS_CODE
+    """
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return f"{timestamp} - {ip_address} - \"GET {url} HTTP/1.1\" {status_code}\n"
 
 def generate_log_file(filename="access.log", num_entries=100):
-    """Generates a sample log file."""
-    ip_addresses = ["192.168.1." + str(i) for i in range(1, 25)]
+    """
+    Generates a sample log file with the specified number of entries.
+
+    Parameters:
+    - filename: Name of the log file to write to.
+    - num_entries: Number of log entries to generate.
+    """
+    ip_addresses = [f"192.168.1.{i}" for i in range(1, 25)]
     urls = ["/page1", "/page2", "/page3", "/images/logo.png", "/api/data"]
     status_codes = [200, 404, 500, 301]
 
@@ -20,8 +29,5 @@ def generate_log_file(filename="access.log", num_entries=100):
             status = random.choice(status_codes)
             f.write(generate_log_entry(ip, url, status))
 
-
-
-# Generate a sample log file (uncomment to create the file)
-generate_log_file()
-
+if __name__ == "__main__":
+    generate_log_file()
